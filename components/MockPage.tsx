2984973dacb37d63f6c5724ef646c4bde42a79a9
Andrew Jose <ajosedev@gmail.com>
2023-02-13 13:12:55 +1100

    MockPage styling

--- components/MockPage.tsx
+++ components/MockPage.tsx
@@ -3,9 +3,12 @@ import styles from '../styles/MockPage.module.css';
 
 export function MockPage() {
     return (
-        <section>
+        <section className={styles.container}>
             <button className={styles.button}>Button</button>
-            <button className={styles.secondaryButton}>Button</button>
+            <button className={`${styles.button} ${styles.secondaryButton}`}>
+                Button
+            </button>
+            <a className={styles.link}>Link</a>
         </section>
     );
 }
